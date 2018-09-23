0165f4008fde2d7db9cea27e3d440e6ebdd7c485
Nick Nance <nance.nick@gmail.com>
2018-09-22 23:06:49 -0700

    minor refactor

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,25 @@
-import { readFileSync } from "fs";
+import { readFile } from "fs";
 import pdf from "pdf-parse";
 
-// tslint:disable:no-console
-const dataBuffer = readFileSync("./fixtures/test.pdf");
-
-pdf(dataBuffer).then((data: any) => {
-    // number of pages
-    console.log(data.numpages);
-    // number of rendered pages
-    console.log(data.numrender);
-    // PDF info
-    console.log(data.info);
-    // PDF metadata
-    console.log(data.metadata);
-    // PDF.js version
-    // check https://mozilla.github.io/pdf.js/getting_started/
-    console.log(data.version);
-    // PDF text
-    console.log(data.text);
+const readFilePromise = (path: string) => new Promise((resolve, reject) => {
+    readFile(path, (err, data) => err ? reject(err) : resolve(data));
 });
+
+// tslint:disable:no-console
+readFilePromise("./fixtures/test.pdf")
+    .then(pdf)
+    .then((data: any) => {
+        // number of pages
+        console.log(data.numpages);
+        // number of rendered pages
+        console.log(data.numrender);
+        // PDF info
+        console.log(data.info);
+        // PDF metadata
+        console.log(data.metadata);
+        // PDF.js version
+        // check https://mozilla.github.io/pdf.js/getting_started/
+        console.log(data.version);
+        // PDF text
+        console.log(data.text);
+    });
